b01ee0068fbf7e740036a4f2ba7f7b5120541ed1
Timo Laitila <timo.t.laitila@gmail.com>
2022-06-05 23:29:03 +0300

    chapter 1.2

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,26 @@
+/*
+ * Rust CLI Book
+ * https://rust-cli.github.io/book/
+ *  
+ */
+
+// command line arguments parser library
+use clap::Parser;
+
+ /// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+ // Struct for command line arguments
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read. Type PathBuf
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("pattern: {}, path: {}", args.pattern, args.path.display());
 }

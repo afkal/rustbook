cbba8ad7d4f744daf27aae98b36903a4d34601b8
Timo Laitila <timo.t.laitila@gmail.com>
2022-06-18 11:40:33 +0300

    largest updated with trait

--- largest/src/main.rs
+++ largest/src/main.rs
@@ -1,3 +1,8 @@
+/*
+ * Rust book - Chapter 10 Traits
+ * https://doc.rust-lang.org/book/ch10-02-traits.html
+ */
+
 
 // i32 specific implementation
 fn largest_i32(list: &[i32]) -> i32 {
@@ -26,7 +31,7 @@ fn largest_char(list: &[char]) -> char {
 }
 
 // generic implementation - works for multiple types
-fn largest<T>(list: &[T]) -> T {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list {
@@ -41,17 +46,17 @@ fn largest<T>(list: &[T]) -> T {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 
 }
